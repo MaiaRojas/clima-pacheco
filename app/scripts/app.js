8c37921628c2ae6c90d07e2ff50eca231c0d2310
MaiaRojas <maia.rt.46@gmail.com>
2017-09-19 21:57:24 -0500

    PRUEBAS

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,13 @@
-const greeting = nombre => console.log(`Hola ${nombre}`);
-
-export default greeting;
+const  mostrarCiudades = ciudades =>{
+    const listadoCiudades=[];
+   ciudades.forEach(ciudad =>{
+     const htmlCiudad= `<h4>${ciudad.nombre}</h4>
+       <p data-id="2643743"><span>9</span>°<img src="http://openweathermap.org/img/w/09d.png" alt=""></p>
+       <p>${ciudad.descripcion}</p>`;
+       console.log(htmlCiudad);
+       listadoCiudades.push(htmlCiudad);
+   });
+  const contenedorCiudades =document.querySelector('.row.marketing');
+  contenedorCiudades.innerHTML=listadoCiudades.join('');
+};
+export default mostrarCiudades;
---
+++ app/scripts/data.js
@@ -0,0 +1,6 @@
+export default[
+  {nombre:'Londres' ,id: 2643743 ,descripcion :"Londres es boni"},
+  {nombre:'Cairo' ,id: 3422 ,descripcion :"Londres es boni"},
+  {nombre:'Sidney' ,id: 3456 ,descripcion :"Londres es boni"},
+  {nombre:'Pekin' ,id: 1816670 ,descripcion :"El clima de Pekín es continental y está influido por su baja altitud (es consecuentemente algo más cálido). Los inviernos son muy fríos y secos, con mínimas que fácilmente descienden a -10 °C o menos, aunque a diferencia con las regiones próximas que se encuentra más al interior y que se encuentran a mayor altitud, las mínimas no son tan bajas y el invierno se hace más soportable."}
+];
